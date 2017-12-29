a0411f291e85174f6a8d66f36c259fba4210c142
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-29 14:17:27 +0100

    add user entity

---
+++ src/main/java/pl/coderslab/entity/User.java
@@ -0,0 +1,85 @@
+package pl.coderslab.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.mindrot.jbcrypt.BCrypt;
+
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+	@NotNull
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotEmpty
+	@Column(unique = true)
+	private String username;
+
+	@NotEmpty
+	private String password;
+
+	@NotNull
+	private boolean enabled;
+
+	@NotEmpty
+	@Email
+	@Column(unique = true)
+	private String email;
+
+	public User() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+	}
+
+	public boolean isPasswordCorrect(String password) {
+		return BCrypt.checkpw(password, this.password);
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
