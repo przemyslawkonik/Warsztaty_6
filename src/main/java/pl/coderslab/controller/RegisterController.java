d11e0af2b0090fee00e5d7cf943b7ffef116cddd
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-29 14:52:11 +0100

    add session manager, user repository, register option

---
+++ src/main/java/pl/coderslab/controller/RegisterController.java
@@ -0,0 +1,43 @@
+package pl.coderslab.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.coderslab.bean.SessionManager;
+import pl.coderslab.entity.User;
+import pl.coderslab.repository.UserRepository;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+	private UserRepository userRep;
+
+	@Autowired
+	public RegisterController(UserRepository userRep) {
+		this.userRep = userRep;
+	}
+
+	@GetMapping("")
+	public String register(Model m) {
+		m.addAttribute("user", new User());
+		return "register";
+	}
+
+	@PostMapping("")
+	public String register(@Valid @ModelAttribute User u, BindingResult br) {
+		if (br.hasErrors()) {
+			return "register";
+		}
+		userRep.save(u);
+		SessionManager.session().setAttribute("user", u);
+		return "redirect:/";
+	}
+}
