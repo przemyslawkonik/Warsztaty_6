7fa843e7ace69a156ff9432a74077e08e50ac193
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-29 16:58:32 +0100

    extract tweet controller and add tweet view, add info which user is currently logged, clean up

--- src/main/java/pl/coderslab/controller/HomeController.java
+++ src/main/java/pl/coderslab/controller/HomeController.java
@@ -1,19 +1,12 @@
 package pl.coderslab.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import pl.coderslab.bean.SessionManager;
 import pl.coderslab.entity.Tweet;
-import pl.coderslab.entity.User;
 import pl.coderslab.repository.TweetRepository;
 
 @Controller
@@ -33,13 +26,4 @@ public class HomeController {
 		return "home";
 	}
 
-	@PostMapping("")
-	public String addTweet(@Valid @ModelAttribute Tweet t, BindingResult br) {
-		if (br.hasErrors()) {
-			return "home";
-		}
-		t.setUser((User) SessionManager.session().getAttribute("user"));
-		tweetRep.save(t);
-		return "redirect:/";
-	}
 }
--- src/main/webapp/WEB-INF/views/user_details.jsp
+++ src/main/webapp/WEB-INF/views/user_details.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8" isELIgnored="false" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -11,7 +12,7 @@
  <%@ include file="jspf/header.jspf" %>
  <%@ include file="jspf/main_menu.jspf" %>
  <p>User: ${user.username}</p> 
- <%@ include file="jspf/tweetList.jspf" %> 
+ <%@ include file="jspf/tweet_list.jspf" %> 
  <%@ include file="jspf/footer.jspf" %>
 </body>
 </html>
