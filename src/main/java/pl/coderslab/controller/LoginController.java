1263584284c5391f6aeed60c9b9bbaed93ba149d
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-29 15:00:09 +0100

    add login option

---
+++ src/main/java/pl/coderslab/controller/LoginController.java
@@ -0,0 +1,43 @@
+package pl.coderslab.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.coderslab.bean.LoginData;
+import pl.coderslab.bean.SessionManager;
+import pl.coderslab.entity.User;
+import pl.coderslab.repository.UserRepository;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	private UserRepository userRep;
+
+	@Autowired
+	public LoginController(UserRepository userRep) {
+		this.userRep = userRep;
+	}
+
+	@GetMapping("")
+	public String login(Model m) {
+		m.addAttribute("loginData", new LoginData());
+		return "login";
+	}
+
+	@PostMapping("")
+	public String login(@ModelAttribute LoginData ld, Model m) {
+		User u = userRep.findOneByEmail(ld.getEmail());
+		if (u != null && u.isPasswordCorrect(ld.getPassword())) {
+			SessionManager.session().setAttribute("user", u);
+			return "redirect:/";
+		}
+		m.addAttribute("errorMsg", "Incorrect data");
+		return "login";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,22 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" isELIgnored="false" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<%@ include file="jspf/head_config.jspf" %>
+<title>Warsztaty 6</title>
+</head>
+<body>
+ <%@ include file="jspf/header.jspf" %>
+ <%@ include file="jspf/main_menu.jspf" %> 
+ <p>${errorMsg}</p>
+ <form:form method="post" modelAttribute="loginData">
+  <p><form:input path="email" placeholder="email"/></p>
+  <p><form:input path="password" type="password" placeholder="password"/></p>
+  <p><input type="submit" value="Log in"/></p>
+ </form:form> 
+ <%@ include file="jspf/footer.jspf" %>
+</body>
+</html>
