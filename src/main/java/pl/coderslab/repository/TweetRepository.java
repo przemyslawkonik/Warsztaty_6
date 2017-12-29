a0ddb039cc0c0e914b6a4ccd673559c4611db794
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-29 15:15:26 +0100

    create tweet repository

---
+++ src/main/java/pl/coderslab/repository/TweetRepository.java
@@ -0,0 +1,16 @@
+package pl.coderslab.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pl.coderslab.entity.Tweet;
+import pl.coderslab.entity.User;
+
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
+	public List<Tweet> findByUser(User u);
+
+	public List<Tweet> findByUserId(long id);
+
+}
