2009bc19118f1a0222a02dd7496e5418365a6382
Przemysław Konik <przemyslawkonik@gmail.com>
2017-12-29 15:59:17 +0100

    update home page by options to add new tweet and browse existing tweets

--- src/main/java/pl/coderslab/controller/HomeController.java
+++ src/main/java/pl/coderslab/controller/HomeController.java
@@ -1,15 +1,45 @@
 package pl.coderslab.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.coderslab.bean.SessionManager;
+import pl.coderslab.entity.Tweet;
+import pl.coderslab.entity.User;
+import pl.coderslab.repository.TweetRepository;
 
 @Controller
 @RequestMapping("")
 public class HomeController {
+	private TweetRepository tweetRep;
+
+	@Autowired
+	public HomeController(TweetRepository tweetRep) {
+		this.tweetRep = tweetRep;
+	}
 
 	@GetMapping("")
-	public String home() {
+	public String home(Model m) {
+		m.addAttribute("tweets", tweetRep.findAllByOrderByCreatedDesc());
+		m.addAttribute("tweet", new Tweet());
 		return "home";
+	}
+
+	@PostMapping("")
+	public String addTweet(@Valid @ModelAttribute Tweet t, BindingResult br) {
+		if (br.hasErrors()) {
+			return "home";
+		}
+		t.setUser((User) SessionManager.session().getAttribute("user"));
+		tweetRep.save(t);
+		return "redirect:/";
 	}
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8" isELIgnored="false" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -10,6 +12,13 @@
 <body>
  <%@ include file="jspf/header.jspf" %>
  <%@ include file="jspf/main_menu.jspf" %>
+ <c:if test="${sessionScope.user!=null}">
+  <form:form method="post" modelAttribute="tweet">
+  <p><form:textarea path="text" placeholder="insert text..."/></p><form:errors path="text"></form:errors>
+  <p><input type="submit" value="Add tweet"/></p>
+  </form:form>
+ </c:if> 
+ <%@ include file="jspf/tweetList.jspf" %>
  <%@ include file="jspf/footer.jspf" %>
 </body>
 </html>
