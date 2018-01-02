f88afdde37a09fffa49bca5906efde8f21282ac8
Przemysław Konik <przemyslawkonik@gmail.com>
2018-01-02 16:13:55 +0100

    modify configuration

--- src/main/java/pl/coderslab/config/AppConfig.java
+++ src/main/java/pl/coderslab/config/AppConfig.java
@@ -1,9 +1,6 @@
 package pl.coderslab.config;
 
-import java.util.Locale;
-
 import javax.persistence.EntityManagerFactory;
-import javax.validation.Validator;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,14 +10,9 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -34,24 +26,6 @@ import pl.coderslab.converter.UserConverter;
 @EnableJpaRepositories(basePackages = { "pl.coderslab.repository" })
 public class AppConfig extends WebMvcConfigurerAdapter {
 
-	@Bean(name = "localeResolver")
-	public LocaleContextResolver getLocaleContextResolver() {
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-		localeResolver.setDefaultLocale(new Locale("pl", "PL"));
-		return localeResolver;
-	}
-
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		super.addResourceHandlers(registry);
-		registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/resources/");
-	}
-
-	@Bean
-	public Validator validator() {
-		return new LocalValidatorFactoryBean();
-	}
-
 	@Bean
 	public TweetConverter tweetConverter() {
 		return new TweetConverter();
@@ -60,6 +34,12 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public UserConverter userConverter() {
 		return new UserConverter();
+	}
+
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addConverter(tweetConverter());
+		registry.addConverter(userConverter());
 	}
 
 	@Bean
@@ -82,17 +62,6 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
 		JpaTransactionManager tm = new JpaTransactionManager(emf);
 		return tm;
-	}
-
-	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
-	}
-
-	@Override
-	public void addFormatters(FormatterRegistry registry) {
-		registry.addConverter(tweetConverter());
-		registry.addConverter(userConverter());
 	}
 
 }
